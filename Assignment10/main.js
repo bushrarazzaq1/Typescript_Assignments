a73adf1f79d07825e813bd54f52f1440bb83224b
Your Name <you@example.com>
2024-03-16 02:42:26 -0800

    first commit

---
+++ Assignment10/main.js
@@ -0,0 +1,7 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Store the name "bushra" in a variable called Name
+var Name = "bushra";
+console.log(Name);
+// Store the current date "24-2-2024" in a variable called currentDate
+var currentDate = "24-2-2024";
+console.log(currentDate);
---
+++ Assignment9/main.js
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoriteNumber = 8;
+var message = "My Favourite Number is:";
+console.log("".concat(message, " ").concat(favoriteNumber));
